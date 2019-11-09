9dab98aecdc52096e9a181ecf5b8073b8d0a8e85
Yiğit KARAKOYUN <kodsever@outlook.com>
2019-11-09 20:43:41 +0300

    cs

---
+++ ConsoleKelebekCizme/ConsoleKelebekCizme/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ConsoleKelebekCizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //15 Değeriyle Çiz Bebeğim :-)
+            Console.WriteLine("----Kelebek----");
+            printKelebekCiz(15);
+
+            Console.Read();
+        }
+        static void printYildiz()
+        {
+            Console.Write("*");
+        }
+        static void printBosluk()
+        {
+            Console.Write(" ");
+        }
+        static void println()
+        {
+            Console.WriteLine();
+        }
+        static void printKelebekString(int bosluk1, int bosluk2, int bosluk3)
+        {
+            printBoslukYildiz(bosluk1);
+            printBoslukYildiz(bosluk2);
+            printBoslukYildiz(bosluk3);
+        }
+        static void printBoslukYildiz(int bosluk)
+        {
+            for (int i = 0; i < bosluk; i++)
+            {
+                printBosluk();
+            }
+            printYildiz();
+        }
+        static void printKelebekCiz(int kadar)
+        {
+            int yarisi = (kadar - 3) / 2;
+
+            for (int i = 0; i <= yarisi; i++)
+            {
+                int bosluk = yarisi - i;
+                printKelebekString(i, bosluk, bosluk);
+                println();
+            }
+            int yatayCizgi = kadar;
+            if (kadar % 2 == 0)
+            {
+                yatayCizgi = kadar - 1;
+            }
+            for (int i = 0; i < yatayCizgi; i++) printYildiz();
+
+            for (int i = yarisi; i >= 0; i--)
+            {
+
+                println();
+                int bosluk = yarisi - i;
+                printKelebekString(i, bosluk, bosluk);
+
+            }
+            println();
+            println();
+        }
+
+
+    }
+}
