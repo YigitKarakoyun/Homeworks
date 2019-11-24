97894f0d1e33cb93b730123c02157046dd52eedd
Yiğit KARAKOYUN <kodsever@outlook.com>
2019-11-25 00:23:09 +0300

    change_desktop_wallpaper

---
+++ Change_DesktopBackground/Change_DesktopBackground/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Change_DesktopBackground
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChangeDesktopBackground = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnChangeDesktopBackground
+            // 
+            this.btnChangeDesktopBackground.Font = new System.Drawing.Font("Yu Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnChangeDesktopBackground.Location = new System.Drawing.Point(40, 12);
+            this.btnChangeDesktopBackground.Name = "btnChangeDesktopBackground";
+            this.btnChangeDesktopBackground.Size = new System.Drawing.Size(173, 114);
+            this.btnChangeDesktopBackground.TabIndex = 0;
+            this.btnChangeDesktopBackground.Text = "Change Desktop Background";
+            this.btnChangeDesktopBackground.UseVisualStyleBackColor = true;
+            this.btnChangeDesktopBackground.Click += new System.EventHandler(this.btnChangeDesktopBackground_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnChangeDesktopBackground);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChangeDesktopBackground;
+    }
+}
+
---
+++ Change_DesktopBackground/Change_DesktopBackground/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Change_DesktopBackground
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int iBackground = 0;
+        private void changeDesktopBackground()
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string[] temp = Directory.GetFiles(path);
+            if (temp == null || 0 >= temp.Length)
+            {
+                return;
+            }
+
+            iBackground++;
+            if (iBackground >= temp.Length)
+            {
+                iBackground = 0;
+            }
+            string file_fullpath_name = temp[iBackground];
+
+            setDesktopBackground(file_fullpath_name);
+        }
+        private void setDesktopBackground(string file_fullpath_name)
+        {
+            const int SET_DESKTOP_BACKGROUND = 20;
+            const int UPDATE_INI_FILE = 1;
+            const int SEND_WINDOWS_INI_CHANGE = 2;
+
+            win32.SystemParametersInfo(SET_DESKTOP_BACKGROUND, 0, file_fullpath_name, UPDATE_INI_FILE | SEND_WINDOWS_INI_CHANGE);
+        }
+
+        string path;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            path = Application.StartupPath + "\\Images\\";
+        }
+
+        private void btnChangeDesktopBackground_Click(object sender, EventArgs e)
+        {
+            changeDesktopBackground();
+        }
+    }
+    internal sealed class win32
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        internal static extern int SystemParametersInfo(
+               int uAction,
+               int uParam,
+               String lpvParam,
+               int fuWinIni
+               );
+    }
+}
