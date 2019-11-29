33f892b21999ba25a78a9f73f0eae3de5f0ae9d3
Yiğit KARAKOYUN <kodsever@outlook.com>
2019-11-30 00:20:27 +0300

    cs

---
+++ PiramitSayilar/PiramidSayilar2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PiramidSayilar2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPiramid_Click(object sender, EventArgs e)
+        {
+            print();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            print();
+        }
+        private void print()
+        {
+            class_Piramid piramid = new class_Piramid();
+
+            int sayi = (int)numericUpDown1.Value;
+            piramid.setDiziOlusturup_Doldurur(sayi);
+            piramid.setDiziDuzenle();
+            string print = piramid.getPrint();
+            label1.Text = print;
+        }
+    }
+}
---
+++ PiramitSayilar/PiramidSayilar2/class_Piramid.cs
@@ -0,0 +1,75 @@
+﻿namespace PiramidSayilar2
+{
+    class class_Piramid
+    {
+        //TEK SATIR DÖNDÜRÜR
+        public string getSatirString(int sayi)
+        {
+            string tempString = sayi.ToString();
+            for (int i = sayi - 1; i > 0; i--)
+            {
+                tempString += " " + i;
+            }
+            for (int i = 2; i < sayi + 1; i++)
+            {
+                tempString += " " + i;
+            }
+            return tempString;
+        }
+        #region DİZİYİ OLUŞTURUR
+        public string[] dizi { get; set; }
+        public int diziBoyut { get; set; }
+        public void setDiziOlusturup_Doldurur(int sayi)
+        {
+            dizi = new string[sayi];
+            diziBoyut = sayi;
+
+            for (int i = 1; i < sayi + 1; i++)
+            {
+                dizi[i - 1] = getSatirString(i).Trim();
+            }
+        }
+        #endregion
+
+        public void setDiziDuzenle()
+        {
+            for (int i = 0; i < diziBoyut; i++)
+            {
+                int solBosluk = ((dizi[diziBoyut - 1].Length - dizi[i].Length) / 2) + (diziBoyut - i - 1);
+
+                if (i < 9 && diziBoyut > 9)
+                {
+                    dizi[i] = getBosluk(solBosluk + (diziBoyut - 9)) + dizi[i];
+                }
+                else if (i >= 9 && i < diziBoyut - 1)
+                {
+                    dizi[i] = getBosluk(solBosluk + (diziBoyut - i - 1)) + dizi[i];
+                }
+                else
+                {
+                    dizi[i] = getBosluk(solBosluk) + dizi[i];
+                }
+                dizi[i] += " bosluk: "+solBosluk;
+            }
+        }
+        private string getBosluk(int solBosluk)
+        {
+            string temp = "";
+            for (int i = 0; i < solBosluk; i++)
+            {
+                temp += " ";
+            }
+            return temp;
+        }
+
+        public string getPrint()
+        {
+            string temp = "";
+            for (int i = 0; i < diziBoyut; i++)
+            {
+                temp += dizi[i] + "\n";
+            }
+            return temp;
+        }
+    }
+}
