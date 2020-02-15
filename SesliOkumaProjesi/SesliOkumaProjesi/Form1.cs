5cf9b25fb3c254d0f1c77e061321c19feafac093
Yiğit KARAKOYUN <kodsever@outlook.com>
2020-02-15 22:25:25 +0300

    cs

---
+++ SesliOkumaProjesi/SesliOkumaProjesi/Form1.cs
@@ -0,0 +1,19 @@
+﻿using SpeechLib;
+using System;
+using System.Windows.Forms;
+namespace SesliOkumaProjesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Com-Speach-alttaki seç
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SpVoice voice = new SpVoice();
+            voice.Speak(richTextBox1.Text);
+        }
+    }
+}
