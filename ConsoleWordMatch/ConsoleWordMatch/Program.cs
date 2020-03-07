e82b933af813bd1f2429b403dca94f4c2763cb24
Yiğit KARAKOYUN <kodsever@outlook.com>
2020-03-07 22:38:18 +0300

    cs

---
+++ ConsoleWordMatch/ConsoleWordMatch/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleWordMatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------------");
+            Jokerli_Permutasyon jip = new Jokerli_Permutasyon();
+            jip.JokerlerDoldur();
+            var result = jip.Jokerli_UcBasamaklilar_Listesi();
+            jip.Yazdir(result);
+
+            Console.Read();
+        }
+    }
+
+}
