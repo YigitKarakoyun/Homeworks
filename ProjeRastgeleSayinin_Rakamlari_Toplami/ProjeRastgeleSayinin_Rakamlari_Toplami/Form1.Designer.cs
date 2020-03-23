230b738c0dd3921987f2db329aaf8082adb27d08
Yiğit KARAKOYUN <kodsever@outlook.com>
2020-03-23 22:02:02 +0300

    cs

---
+++ ProjeRastgeleSayinin_Rakamlari_Toplami/ProjeRastgeleSayinin_Rakamlari_Toplami/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace ProjeRastgeleSayinin_Rakamlari_Toplami
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRastgeleSayiOlustur = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRastgeleSayiOlustur
+            // 
+            this.btnRastgeleSayiOlustur.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnRastgeleSayiOlustur.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRastgeleSayiOlustur.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F);
+            this.btnRastgeleSayiOlustur.Location = new System.Drawing.Point(11, 12);
+            this.btnRastgeleSayiOlustur.Name = "btnRastgeleSayiOlustur";
+            this.btnRastgeleSayiOlustur.Size = new System.Drawing.Size(425, 66);
+            this.btnRastgeleSayiOlustur.TabIndex = 0;
+            this.btnRastgeleSayiOlustur.Text = "Rasgele Sayi Oluştur";
+            this.btnRastgeleSayiOlustur.UseVisualStyleBackColor = false;
+            this.btnRastgeleSayiOlustur.Click += new System.EventHandler(this.btnRastgeleSayiOlustur_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(9, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(428, 73);
+            this.label1.TabIndex = 1;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 163);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRastgeleSayiOlustur);
+            this.Name = "Form1";
+            this.Text = "Rastgele Oluşturulan Sayının Rakamları Toplamı Programı";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRastgeleSayiOlustur;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ProjeRastgeleSayinin_Rakamlari_Toplami/ProjeRastgeleSayinin_Rakamlari_Toplami/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjeRastgeleSayinin_Rakamlari_Toplami
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRastgeleSayiOlustur_Click(object sender, EventArgs e)
+        {
+            int randomNumber = Get_RandomNumber();
+
+            label1.Text = "";
+            label1.Text = "Rastgele Sayı: "+ randomNumber.ToString();
+            label1.Text += Environment.NewLine;
+            int randomNumberSum = Get_SumNumbers(randomNumber);
+            label1.Text += "Rastgele Sayının Rakamları Toplamı:" + randomNumberSum;
+    
+        }
+        Random rnd = new Random();
+        private int Get_RandomNumber()
+        {
+           return rnd.Next(1000);
+        }
+        private int Get_SumNumbers(int number)
+        {
+            int sum = 0;
+            while (number > 0)
+            {
+                sum += (number % 10);
+                number = number / 10;
+            }
+            return sum;
+        }
+    }
+}
