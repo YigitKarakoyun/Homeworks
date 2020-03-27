869d72d9cfd779224c66a78ca1364630ab0f4e4e
YİĞİT KARAKOYUN <yigitkarakoyun@outlook.com>
2020-03-27 13:46:15 +0300

    Star

---
+++ DrawButterflyUsingStar/ProjectStar/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using ProjectStar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectStar.Controllers
+{
+    public class HomeController : Controller
+    {
+        Kelebek kelebek = new Kelebek();
+        public ActionResult Index()
+        {
+            return View(kelebek.GetKelebekList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DrawButterflyUsingStar/ProjectStar/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjectStar.MvcApplication" Language="C#" %>
