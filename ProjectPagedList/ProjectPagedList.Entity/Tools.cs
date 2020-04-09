e678245ff637bc726832873406fbb0d82e88a803
YİĞİT KARAKOYUN <yigitkarakoyun@outlook.com>
2020-04-09 17:17:09 +0300

    pagedlist

---
+++ ProjectPagedList/ProjectPagedList.Entity/Tools.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPagedList.Entity
+{
+    public class Tools
+    {
+        static DbMeyvelerEntities db = null;
+        public static DbMeyvelerEntities GetMeyvelerEntities()
+        {
+            if (db==null)
+            {
+                db = new DbMeyvelerEntities();
+            }
+            return db;
+        }
+    }
+}
---
+++ ProjectPagedList/ProjectPagedList/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using ProjectPagedList.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+namespace ProjectPagedList.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbMeyvelerEntities db = Tools.GetMeyvelerEntities();
+        public ActionResult Index(int sayfa = 1)
+        {
+            var temp = db.TBL_MEYVELER.ToList().ToPagedList(sayfa,3);
+            return View(temp);
+        }
+    }
+}
